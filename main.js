7d7f7c4c6c911b3d1d9747386fc411e1873f3eb1
shivasai0913 <155218572+shivasai0913@users.noreply.github.com>
2024-06-23 10:08:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// ####################### nav bar //
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+// ################# section //
+let sections = document.querySelector('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset =sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >=offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // @@@@@@@@@@@@@@@@ sticky nav //
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY >100);
+
+    //###################### remove toggle icon and navbar //
+    menuIcon.classList.remove('fax-xmark');
+    navbar.classList.remove('active');
+    
+};
+
+//######### scroll reveal /
+ScrollReveal ( {
+    distance : '80px',
+    duration: 2000, 
+    delay: 200,
+});
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact.form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+// ############### typed js//
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Photograper'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
+        
